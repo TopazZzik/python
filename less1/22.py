6435bc061cfaf10022c1f8ca610e83d8a8b808da
Luda <millya92@gmail.com>
2022-10-09 19:02:10 +0300

    two

---
+++ less1/22.py
@@ -0,0 +1,68 @@
+# Даны два файла, в каждом из которых находится запись многочлена.
+# Задача - сформировать файл, содержащий сумму многочленов.
+
+from pickle import TRUE
+
+
+def readEquation(path):
+    file = open (path, "r")
+    firstEquation =  file.read()
+    file.close()
+    eqation = {}
+
+
+    firstEquation = firstEquation.replace(" + ", " +").replace(" - ", " -").split()[:-2]
+    for i in range(len(firstEquation)):
+        firstEquation[i] = firstEquation[i].replace("+", "").split("x^")
+        if firstEquation[i][0] == "":
+           firstEquation[i][0] = '1' 
+        if len(firstEquation[i]) < 2:
+            firstEquation[i].append('0')
+          
+        eqation[int(firstEquation[i][1])] = int(firstEquation[i][0])
+
+    return eqation
+
+finalWord = {}
+
+word1 = readEquation(r"numbers1.txt")
+word2 = readEquation(r"numbers2.txt")
+
+
+for i in range(max(len(word1), len(word2)) + 1, -1, -1):
+    first = word1.get(i)
+    second = word2.get(i)
+    if first != None or second != None:
+        finalWord[i] = (first if first != None else 0) + (second if second != None else 0)
+
+finalString = ""
+First = True
+for item in finalWord:
+    symbol = "+ "
+    last = "x^"
+    degree = item
+
+    if finalWord[item] < 0:
+        symbol = "- "
+    if First and finalWord[item] >= 0:
+        symbol = ""
+    if First and finalWord[item] < 0:
+        symbol = "-"
+    finalWord[item] = abs(finalWord[item])
+    if finalWord[item] == 1:
+        finalWord[item] = ""
+    
+    if item == 1:
+        last = "x"
+        degree = ""
+    if item < 1:
+        last = ""
+        degree = ""
+    finalString += symbol+str(finalWord[item])+last+str(degree)+" "
+    First = False
+
+finalString += "= 0"
+data = open (r"numbers4.txt", "w")
+data.write(finalString)
+data.close()  
+print (finalString)
